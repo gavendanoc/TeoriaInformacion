6a6fa87643a8d76c01f37066ccc5c6e95c1390fd
fpieschaconr <fpieschaconr@unal.edu.co>
2020-06-16 11:32:15 -0500

    lab4 punto2 final

--- lab4/parte2/punto2final.m
+++ lab4/parte2/punto2final.m
@@ -61,12 +61,15 @@ bits_recibidos_izq_a_der=[ 11 01 00 01 11 ];
 distancias = [0 0 0 0];
 
 ##t1 a t2
+##caso 00
 if bits_recibidos_izq_a_der( 1 ) == 0
   distancias( 1 ) = 0;
   distancias( 2 ) = 2;
+##caso 11
 elseif bits_recibidos_izq_a_der( 1 ) == 11
   distancias( 1 ) = 2;
   distancias( 2 ) = 0;
+##caso 01 10
 else
   distancias( 1 ) = 1;
   distancias( 2 ) = 1;
@@ -74,21 +77,29 @@ endif
 
 ##t2 a t3
 prev_distancias = distancias;
+nodos1 = [1,2,4];
+nodos2 = [1,2,5];
+nodos3 = [1,3,6];
+nodos4 = [1,3,7];
+##caso 11
 if bits_recibidos_izq_a_der( 2 ) == 11
   distancias( 1 ) = prev_distancias( 1 ) + 2;
   distancias( 2 ) = prev_distancias( 1 ) + 0;
   distancias( 3 ) = prev_distancias( 2 ) + 1;
   distancias( 4 ) = prev_distancias( 2 ) + 1;
+##caso 10  
 elseif bits_recibidos_izq_a_der( 2 ) == 10
   distancias( 1 ) = prev_distancias( 1 ) + 1;
   distancias( 2 ) = prev_distancias( 1 ) + 1;
   distancias( 3 ) = prev_distancias( 2 ) + 2;
   distancias( 4 ) = prev_distancias( 2 ) + 0;
+##caso 01  
 elseif bits_recibidos_izq_a_der( 2 ) == 1
   distancias( 1 ) = prev_distancias( 1 ) + 1;
   distancias( 2 ) = prev_distancias( 1 ) + 1;
   distancias( 3 ) = prev_distancias( 2 ) + 0;
   distancias( 4 ) = prev_distancias( 2 ) + 2;
+##caso 00  
 else
   distancias( 1 ) = prev_distancias( 1 ) + 0;
   distancias( 2 ) = prev_distancias( 1 ) + 2;
@@ -96,559 +107,100 @@ else
   distancias( 4 ) = prev_distancias( 2 ) + 1;
 endif
 
-nodos1 = [1,2,4];
-nodos2 = [1,2,5];
-nodos3 = [1,3,6];
-nodos4 = [1,3,7];
+for l=3:5 ##misma logica para t3 a t4 a t5 a t6
+  prev_distancias = distancias;
+  prev_nodos1 = nodos1;
+  prev_nodos2 = nodos2;
+  prev_nodos3 = nodos3;
+  prev_nodos4 = nodos4;
+  
+  a00=99;
+  a10=99;
+  a01=99;
+  a11=99;
+  if bits_recibidos_izq_a_der( l ) == 11 ##distancias si recibe 11
+    a00=2;
+    a10=1;
+    a01=1;
+    a11=0;
+  elseif bits_recibidos_izq_a_der( l ) == 10 ##distancias si recibe 10
+    a00=1;
+    a10=0;
+    a01=2;
+    a11=1;
+  elseif bits_recibidos_izq_a_der( l ) == 1 ##distancias si recibe 01
+    a00=1;
+    a10=2;
+    a01=0;
+    a11=1;
+  else ##distancias si recibe 00
+    a00=0;
+    a10=1;
+    a01=1;
+    a11=2;
+  endif
+  
+  ##a estado 00
+  opcion1 = prev_distancias( 1 ) + a00;##00
+  opcion2 = prev_distancias( 3 ) + a11;##11
+  if opcion1 < opcion2
+    nodos1 = [ prev_nodos1, 8+(4*(l-3))];
+    distancias( 1 ) = opcion1;
+  else
+    nodos1 = [ prev_nodos3, 8+(4*(l-3))];
+    distancias( 1 ) = opcion2;
+  endif
+  ##a estado 10
+  opcion1 = prev_distancias( 1 ) + a11;##11
+  opcion2 = prev_distancias( 3 ) + a00;##00
+  if opcion1 < opcion2
+    nodos2 = [ prev_nodos1, 9+(4*(l-3))];
+    distancias( 2 ) = opcion1;
+  else
+    nodos2 = [ prev_nodos3, 9+(4*(l-3))];
+    distancias( 2 ) = opcion2;
+  endif
+  ##a estado 01
+  opcion1 = prev_distancias( 2 ) + a01;##01
+  opcion2 = prev_distancias( 4 ) + a10;##10
+  if opcion1 < opcion2
+    nodos3 = [ prev_nodos2, 10+(4*(l-3))];
+    distancias( 3 ) = opcion1;
+  else
+    nodos3 = [ prev_nodos4, 10+(4*(l-3))];
+    distancias( 3 ) = opcion2;
+  endif
+  ##a estado 11
+  opcion1 = prev_distancias( 2 ) + a10;##10
+  opcion2 = prev_distancias( 4 ) + a01;##01
+  if opcion1 < opcion2
+    nodos4 = [ prev_nodos2, 11+(4*(l-3))];
+    distancias( 4 ) = opcion1;
+  else
+    nodos4 = [ prev_nodos4, 11+(4*(l-3))];
+    distancias( 4 ) = opcion2;
+  endif
+endfor
 
-##t3 a t4
-prev_distancias = distancias;
-nods1 = nodos1;
-nods2 = nodos2;
-nods3 = nodos3;
-nods4 = nodos4;
-##Caso 11
-if bits_recibidos_izq_a_der( 3 ) == 11
-  ##prev_distancias 00
-  opcion1 = prev_distancias( 1 ) + 2;
-  opcion2 = prev_distancias( 3 ) + 0;
-  if opcion1 < opcion2
-    nodos1 = [ nods1, 8];
-    distancias( 1 ) = opcion1;
-  else
-    nodos1 = [ nods3, 8];
-    distancias( 1 ) = opcion2;
-  endif
-  ##prev_distancias 10
-  opcion1 = prev_distancias( 1 ) + 0;
-  opcion2 = prev_distancias( 3 ) + 2;
-  if opcion1 < opcion2
-    nodos2 = [ nods1, 9];
-    distancias( 2 ) = opcion1;
-  else
-    nodos2 = [ nods3, 9];
-    distancias( 2 ) = opcion2;
-  endif
-  ##prev_distancias 01
-  opcion1 = prev_distancias( 2 ) + 1;
-  opcion2 = prev_distancias( 4 ) + 1;
-  if opcion1 < opcion2
-    nodos3 = [ nods2, 10];
-    distancias( 3 ) = opcion1;
-  else
-    nodos3 = [ nods4, 10];
-    distancias( 3 ) = opcion2;
-  endif
-  ##prev_distancias 11
-  opcion1 = prev_distancias( 2 ) + 1;
-  opcion2 = prev_distancias( 4 ) + 1;
-  if opcion1 < opcion2
-    nodos4 = [ nods2, 11];
-    distancias( 4 ) = opcion1;
-  else
-    nodos4 = [ nods4, 11];
-    distancias( 4 ) = opcion2;
-  endif
-##Caso 10
-elseif bits_recibidos_izq_a_der( 3 ) == 10
-  ##prev_distancias 00
-  opcion1 = prev_distancias( 1 ) + 1;
-  opcion2 = prev_distancias( 3 ) + 1;
-  if opcion1 < opcion2
-    nodos1 = [ nods1, 8];
-    distancias( 1 ) = opcion1;
-  else
-    nodos1 = [ nods3, 8];
-    distancias( 1 ) = opcion2;
-  endif
-  ##prev_distancias 10
-  opcion1 = prev_distancias( 1 ) + 1;
-  opcion2 = prev_distancias( 3 ) + 1;
-  if opcion1 < opcion2
-    nodos2 = [ nods1, 9];
-    distancias( 2 ) = opcion1;
-  else
-    nodos2 = [ nods3, 9];
-    distancias( 2 ) = opcion2;
-  endif
-  ##prev_distancias 01
-  opcion1 = prev_distancias( 2 ) + 2;
-  opcion2 = prev_distancias( 4 ) + 0;
-  if opcion1 < opcion2
-    nodos3 = [ nods2, 10];
-    distancias( 3 ) = opcion1;
-  else
-    nodos3 = [ nods4, 10];
-    distancias( 3 ) = opcion2;
-  endif
-  ##prev_distancias 11
-  opcion1 = prev_distancias( 2 ) + 0;
-  opcion2 = prev_distancias( 4 ) + 2;
-  if opcion1 < opcion2
-    nodos4 = [ nods2, 11];
-    distancias( 4 ) = opcion1;
-  else
-    nodos4 = [ nods4, 11];
-    distancias( 4 ) = opcion2;
-  endif
-##Caso 01
-elseif bits_recibidos_izq_a_der( 3 ) == 1
-  ##prev_distancias 00
-  opcion1 = prev_distancias( 1 ) + 1;
-  opcion2 = prev_distancias( 3 ) + 1;
-  if opcion1 < opcion2
-    nodos1 = [ nods1, 8];
-    distancias( 1 ) = opcion1;
-  else
-    nodos1 = [ nods3, 8];
-    distancias( 1 ) = opcion2;
-  endif
-  ##prev_distancias 10
-  opcion1 = prev_distancias( 1 ) + 1;
-  opcion2 = prev_distancias( 3 ) + 1;
-  if opcion1 < opcion2
-    nodos2 = [ nods1, 9];
-    distancias( 2 ) = opcion1;
-  else
-    nodos2 = [ nods3, 9];
-    distancias( 2 ) = opcion2;
-  endif
-  ##prev_distancias 01
-  opcion1 = prev_distancias( 2 ) + 0;
-  opcion2 = prev_distancias( 4 ) + 2;
-  if opcion1 < opcion2
-    nodos3 = [ nods2, 10];
-    distancias( 3 ) = opcion1;
-  else
-    nodos3 = [ nods4, 10];
-    distancias( 3 ) = opcion2;
-  endif
-  ##prev_distancias 11
-  opcion1 = prev_distancias( 2 ) + 2;
-  opcion2 = prev_distancias( 4 ) + 0;
-  if opcion1 < opcion2
-    nodos4 = [ nods2, 11];
-    distancias( 4 ) = opcion1;
-  else
-    nodos4 = [ nods4, 11];
-    distancias( 4 ) = opcion2;
-  endif
-##Caso 00
-else
-  ##prev_distancias 00
-  opcion1 = prev_distancias( 1 ) + 0;
-  opcion2 = prev_distancias( 3 ) + 2;
-  if opcion1 < opcion2
-    nodos1 = [ nods1, 8];
-    distancias( 1 ) = opcion1;
-  else
-    nodos1 = [ nods3, 8];
-    distancias( 1 ) = opcion2;
-  endif
-  ##prev_distancias 10
-  opcion1 = prev_distancias( 1 ) + 2;
-  opcion2 = prev_distancias( 3 ) + 0;
-  if opcion1 < opcion2
-    nodos2 = [ nods1, 9];
-    distancias( 2 ) = opcion1;
-  else
-    nodos2 = [ nods3, 9];
-    distancias( 2 ) = opcion2;
-  endif
-  ##prev_distancias 01
-  opcion1 = prev_distancias( 2 ) + 1;
-  opcion2 = prev_distancias( 4 ) + 1;
-  if opcion1 < opcion2
-    nodos3 = [ nods2, 10];
-    distancias( 3 ) = opcion1;
-  else
-    nodos3 = [ nods4, 10];
-    distancias( 3 ) = opcion2;
-  endif
-  ##prev_distancias 11
-  opcion1 = prev_distancias( 2 ) + 1;
-  opcion2 = prev_distancias( 4 ) + 1;
-  if opcion1 < opcion2
-    nodos4 = [ nods2, 11];
-    distancias( 4 ) = opcion1;
-  else
-    nodos4 = [ nods4, 11];
-    distancias( 4 ) = opcion2;
-  endif
-endif
+camino_viterbi = [];
 
-
-
-
-##t4 a t5
-prev_distancias = distancias;
-nods1 = nodos1;
-nods2 = nodos2;
-nods3 = nodos3;
-nods4 = nodos4;
-##Caso 11
-if bits_recibidos_izq_a_der( 4 ) == 11
-  ##prev_distancias 00
-  opcion1 = prev_distancias( 1 ) + 2;
-  opcion2 = prev_distancias( 3 ) + 0;
-  if opcion1 < opcion2
-    nodos1 = [ nods1, 12];
-    distancias( 1 ) = opcion1;
-  else
-    nodos1 = [ nods3, 12];
-    distancias( 1 ) = opcion2;
-  endif
-  ##prev_distancias 10
-  opcion1 = prev_distancias( 1 ) + 0;
-  opcion2 = prev_distancias( 3 ) + 2;
-  if opcion1 < opcion2
-    nodos2 = [ nods1, 13];
-    distancias( 2 ) = opcion1;
-  else
-    nodos2 = [ nods3, 13];
-    distancias( 2 ) = opcion2;
-  endif
-  ##prev_distancias 01
-  opcion1 = prev_distancias( 2 ) + 1;
-  opcion2 = prev_distancias( 4 ) + 1;
-  if opcion1 < opcion2
-    nodos3 = [ nods2, 14];
-    distancias( 3 ) = opcion1;
-  else
-    nodos3 = [ nods4, 14];
-    distancias( 3 ) = opcion2;
-  endif
-  ##prev_distancias 11
-  opcion1 = prev_distancias( 2 ) + 1;
-  opcion2 = prev_distancias( 4 ) + 1;
-  if opcion1 < opcion2
-    nodos4 = [ nods2, 15];
-    distancias( 4 ) = opcion1;
-  else
-    nodos4 = [ nods4, 15];
-    distancias( 4 ) = opcion2;
-  endif
-##Caso 10
-elseif bits_recibidos_izq_a_der( 4 ) == 10
-  ##prev_distancias 00
-  opcion1 = prev_distancias( 1 ) + 1;
-  opcion2 = prev_distancias( 3 ) + 1;
-  if opcion1 < opcion2
-    nodos1 = [ nods1, 12];
-    distancias( 1 ) = opcion1;
-  else
-    nodos1 = [ nods3, 12];
-    distancias( 1 ) = opcion2;
-  endif
-  ##prev_distancias 10
-  opcion1 = prev_distancias( 1 ) + 1;
-  opcion2 = prev_distancias( 3 ) + 1;
-  if opcion1 < opcion2
-    nodos2 = [ nods1, 13];
-    distancias( 2 ) = opcion1;
-  else
-    nodos2 = [ nods3, 13];
-    distancias( 2 ) = opcion2;
-  endif
-  ##prev_distancias 01
-  opcion1 = prev_distancias( 2 ) + 2;
-  opcion2 = prev_distancias( 4 ) + 0;
-  if opcion1 < opcion2
-    nodos3 = [ nods2, 14];
-    distancias( 3 ) = opcion1;
-  else
-    nodos3 = [ nods4, 14];
-    distancias( 3 ) = opcion2;
-  endif
-  ##prev_distancias 11
-  opcion1 = prev_distancias( 2 ) + 0;
-  opcion2 = prev_distancias( 4 ) + 2;
-  if opcion1 < opcion2
-    nodos4 = [ nods2, 15];
-    distancias( 4 ) = opcion1;
-  else
-    nodos4 = [ nods4, 15];
-    distancias( 4 ) = opcion2;
-  endif
-##Caso 01
-elseif bits_recibidos_izq_a_der( 4 ) == 1
-  ##prev_distancias 00
-  opcion1 = prev_distancias( 1 ) + 1;
-  opcion2 = prev_distancias( 3 ) + 1;
-  if opcion1 < opcion2
-    nodos1 = [ nods1, 12];
-    distancias( 1 ) = opcion1;
-  else
-    nodos1 = [ nods3, 12];
-    distancias( 1 ) = opcion2;
-  endif
-  ##prev_distancias 10
-  opcion1 = prev_distancias( 1 ) + 1;
-  opcion2 = prev_distancias( 3 ) + 1;
-  if opcion1 < opcion2
-    nodos2 = [ nods1, 13];
-    distancias( 2 ) = opcion1;
-  else
-    nodos2 = [ nods3, 13];
-    distancias( 2 ) = opcion2;
-  endif
-  ##prev_distancias 01
-  opcion1 = prev_distancias( 2 ) + 0;
-  opcion2 = prev_distancias( 4 ) + 2;
-  if opcion1 < opcion2
-    nodos3 = [ nods2, 14];
-    distancias( 3 ) = opcion1;
-  else
-    nodos3 = [ nods4, 14];
-    distancias( 3 ) = opcion2;
-  endif
-  ##prev_distancias 11
-  opcion1 = prev_distancias( 2 ) + 2;
-  opcion2 = prev_distancias( 4 ) + 0;
-  if opcion1 < opcion2
-    nodos4 = [ nods2, 15];
-    distancias( 4 ) = opcion1;
-  else
-    nodos4 = [ nods4, 15];
-    distancias( 4 ) = opcion2;
-  endif
-##Caso 00
-else
-  ##prev_distancias 00
-  opcion1 = prev_distancias( 1 ) + 0;
-  opcion2 = prev_distancias( 3 ) + 2;
-  if opcion1 < opcion2
-    nodos1 = [ nods1, 12];
-    distancias( 1 ) = opcion1;
-  else
-    nodos1 = [ nods3, 12];
-    distancias( 1 ) = opcion2;
-  endif
-  ##prev_distancias 10
-  opcion1 = prev_distancias( 1 ) + 2;
-  opcion2 = prev_distancias( 3 ) + 0;
-  if opcion1 < opcion2
-    nodos2 = [ nods1, 13];
-    distancias( 2 ) = opcion1;
-  else
-    nodos2 = [ nods3, 13];
-    distancias( 2 ) = opcion2;
-  endif
-  ##prev_distancias 01
-  opcion1 = prev_distancias( 2 ) + 1;
-  opcion2 = prev_distancias( 4 ) + 1;
-  if opcion1 < opcion2
-    nodos3 = [ nods2, 14];
-    distancias( 3 ) = opcion1;
-  else
-    nodos3 = [ nods4, 14];
-    distancias( 3 ) = opcion2;
-  endif
-  ##prev_distancias 11
-  opcion1 = prev_distancias( 2 ) + 1;
-  opcion2 = prev_distancias( 4 ) + 1;
-  if opcion1 < opcion2
-    nodos4 = [ nods2, 15];
-    distancias( 4 ) = opcion1;
-  else
-    nodos4 = [ nods4, 15];
-    distancias( 4 ) = opcion2;
-  endif
-endif
-
-
-
-
-##t5 a t6
-prev_distancias = distancias;
-nods1 = nodos1;
-nods2 = nodos2;
-nods3 = nodos3;
-nods4 = nodos4;
-##Caso 11
-if bits_recibidos_izq_a_der( 5 ) == 11
-  ##prev_distancias 00
-  opcion1 = prev_distancias( 1 ) + 2;
-  opcion2 = prev_distancias( 3 ) + 0;
-  if opcion1 < opcion2
-    nodos1 = [ nods1, 16];
-    distancias( 1 ) = opcion1;
-  else
-    nodos1 = [ nods3, 16];
-    distancias( 1 ) = opcion2;
-  endif
-  ##prev_distancias 10
-  opcion1 = prev_distancias( 1 ) + 0;
-  opcion2 = prev_distancias( 3 ) + 2;
-  if opcion1 < opcion2
-    nodos2 = [ nods1, 17];
-    distancias( 2 ) = opcion1;
-  else
-    nodos2 = [ nods3, 17];
-    distancias( 2 ) = opcion2;
-  endif
-  ##prev_distancias 01
-  opcion1 = prev_distancias( 2 ) + 1;
-  opcion2 = prev_distancias( 4 ) + 1;
-  if opcion1 < opcion2
-    nodos3 = [ nods2, 18];
-    distancias( 3 ) = opcion1;
-  else
-    nodos3 = [ nods4, 18];
-    distancias( 3 ) = opcion2;
-  endif
-  ##prev_distancias 11
-  opcion1 = prev_distancias( 2 ) + 1;
-  opcion2 = prev_distancias( 4 ) + 1;
-  if opcion1 < opcion2
-    nodos4 = [ nods2, 19];
-    distancias( 4 ) = opcion1;
-  else
-    nodos4 = [ nods4, 19];
-    distancias( 4 ) = opcion2;
-  endif
-##Caso 10
-elseif bits_recibidos_izq_a_der( 5 ) == 10
-  ##prev_distancias 00
-  opcion1 = prev_distancias( 1 ) + 1;
-  opcion2 = prev_distancias( 3 ) + 1;
-  if opcion1 < opcion2
-    nodos1 = [ nods1, 16];
-    distancias( 1 ) = opcion1;
-  else
-    nodos1 = [ nods3, 16];
-    distancias( 1 ) = opcion2;
-  endif
-  ##prev_distancias 10
-  opcion1 = prev_distancias( 1 ) + 1;
-  opcion2 = prev_distancias( 3 ) + 1;
-  if opcion1 < opcion2
-    nodos2 = [ nods1, 17];
-    distancias( 2 ) = opcion1;
-  else
-    nodos2 = [ nods3, 17];
-    distancias( 2 ) = opcion2;
-  endif
-  ##prev_distancias 01
-  opcion1 = prev_distancias( 2 ) + 2;
-  opcion2 = prev_distancias( 4 ) + 0;
-  if opcion1 < opcion2
-    nodos3 = [ nods2, 18];
-    distancias( 3 ) = opcion1;
-  else
-    nodos3 = [ nods4, 18];
-    distancias( 3 ) = opcion2;
-  endif
-  ##prev_distancias 11
-  opcion1 = prev_distancias( 2 ) + 0;
-  opcion2 = prev_distancias( 4 ) + 2;
-  if opcion1 < opcion2
-    nodos4 = [ nods2, 19];
-    distancias( 4 ) = opcion1;
-  else
-    nodos4 = [ nods4, 19];
-    distancias( 4 ) = opcion2;
-  endif
-##Caso 01
-elseif bits_recibidos_izq_a_der( 5 ) == 1
-  ##prev_distancias 00
-  opcion1 = prev_distancias( 1 ) + 1;
-  opcion2 = prev_distancias( 3 ) + 1;
-  if opcion1 < opcion2
-    nodos1 = [ nods1, 16];
-    distancias( 1 ) = opcion1;
-  else
-    nodos1 = [ nods3, 16];
-    distancias( 1 ) = opcion2;
-  endif
-  ##prev_distancias 10
-  opcion1 = prev_distancias( 1 ) + 1;
-  opcion2 = prev_distancias( 3 ) + 1;
-  if opcion1 < opcion2
-    nodos2 = [ nods1, 17];
-    distancias( 2 ) = opcion1;
-  else
-    nodos2 = [ nods3, 17];
-    distancias( 2 ) = opcion2;
-  endif
-  ##prev_distancias 01
-  opcion1 = prev_distancias( 2 ) + 0;
-  opcion2 = prev_distancias( 4 ) + 2;
-  if opcion1 < opcion2
-    nodos3 = [ nods2, 18];
-    distancias( 3 ) = opcion1;
-  else
-    nodos3 = [ nods4, 18];
-    distancias( 3 ) = opcion2;
-  endif
-  ##prev_distancias 11
-  opcion1 = prev_distancias( 2 ) + 2;
-  opcion2 = prev_distancias( 4 ) + 0;
-  if opcion1 < opcion2
-    nodos4 = [ nods2, 19];
-    distancias( 4 ) = opcion1;
-  else
-    nodos4 = [ nods4, 19];
-    distancias( 4 ) = opcion2;
-  endif
-##Caso 00
-else
-  ##prev_distancias 00
-  opcion1 = prev_distancias( 1 ) + 0;
-  opcion2 = prev_distancias( 3 ) + 2;
-  if opcion1 < opcion2
-    nodos1 = [ nods1, 16];
-    distancias( 1 ) = opcion1;
-  else
-    nodos1 = [ nods3, 16];
-    distancias( 1 ) = opcion2;
-  endif
-  ##prev_distancias 10
-  opcion1 = prev_distancias( 1 ) + 2;
-  opcion2 = prev_distancias( 3 ) + 0;
-  if opcion1 < opcion2
-    nodos2 = [ nods1, 17];
-    distancias( 2 ) = opcion1;
-  else
-    nodos2 = [ nods3, 17];
-    distancias( 2 ) = opcion2;
-  endif
-  ##prev_distancias 01
-  opcion1 = prev_distancias( 2 ) + 1;
-  opcion2 = prev_distancias( 4 ) + 1;
-  if opcion1 < opcion2
-    nodos3 = [ nods2, 18];
-    distancias( 3 ) = opcion1;
-  else
-    nodos3 = [ nods4, 18];
-    distancias( 3 ) = opcion2;
-  endif
-  ##prev_distancias 11
-  opcion1 = prev_distancias( 2 ) + 1;
-  opcion2 = prev_distancias( 4 ) + 1;
-  if opcion1 < opcion2
-    nodos4 = [ nods2, 19];
-    distancias( 4 ) = opcion1;
-  else
-    nodos4 = [ nods4, 19];
-    distancias( 4 ) = opcion2;
-  endif
-endif
-
-lista = [];
-
+printf("Distancia minima\n");
 switch( find( min( distancias ) == distancias ) )
-  case 1
-    lista = nodos1;
-  case 2
-    lista = nodos2;
-  case 3
-    lista = nodos3;
-  otherwise
-    lista = nodos4;
+case 1
+  distancias(1)
+  camino_viterbi = nodos1;
+case 2
+  distancias(2)
+  camino_viterbi = nodos2;
+case 3
+  distancias(3)
+  camino_viterbi = nodos3;
+otherwise
+  distancias(4)
+  camino_viterbi = nodos4;
 endswitch
-printf("Estados recorridos.\n");
-lista
+printf("Estados recorridos\n");
+camino_viterbi
 
 trellis=[
   -1   -1   -1   -1   -1   -1   -1   -1   -1   -1   -1   -1   -1   -1   -1
@@ -674,9 +226,9 @@ trellis=[
 
 bits = [ -1 -1 -1 -1 -1 ];
 for i = 1 : 5
-  bits( i ) = trellis( lista( i +1 ), lista( i ) );
+  bits( i ) = trellis( camino_viterbi( i +1 ), camino_viterbi( i ) );
 endfor
-printf("Cadena original.\n");
+printf("Cadena original\n");
 bits
 
 
